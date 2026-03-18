21c82b639cbc1c3b93f8eebb0e0d1b5182a4d4e6
Rubí <rubivillalonarranz.guadalupe@alumnado.fundacionloyola.net>
2026-03-18 20:38:37 +0100

    Add files via upload

--- pruebas/agradecer.php
+++ pruebas/agradecer.php
@@ -3,30 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" author="Rubí">
-    <title>Agradece en Compañía</title>
+    <title>Agradecer</title>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="estilo.css">
 </head>
 <body>
-
-    <div class="envoltorio-principal">
-        <div class="encabezado">
-            <h1>AGRADECE EN COMPAÑÍA</h1>
-        </div>
-        <nav>
-            <a href="#" class="activo">Agradecer</a>
-            <a href="#">Recibir</a>
-            <a href="#">Cerrar Sesión</a>
-        </nav>
-        <div class="tarjeta-login">
-            <form>
+    <header>
+        <h1>AGRADECE EN COMPAÑÍA</h1>
+    </header>
+    <nav>
+        <a href="#">Agradecer</a>
+        <a href="#">Recibir</a>
+        <a href="#">Cerrar Sesión</a>
+    </nav>
+    <div>
+        <div class="tarjeta">
+            <form action="okformulario.php" method="POST">
                 <div class="grupo-formulario">
                     <label>para</label>
                     <div class="contenedor-entrada">
                         <select>
-                            <?php 
-                                <option value="'valor_campo'" name="'nombre_campo'">.nombre_jesuita.</option>;
-                            ?>        
+                            <!-- <interrogacion php
+                <option value="'valor_campo'" name="'nombre_campo'">.nombre_jesuita.</option>;
+                             interrogacion>      -->  
+
             <!-- En value va el valor del campo, en name el nombre del campo y lo que se visualiza en navegador es el jesuita correspondiente  -->
                         </select>
                     </div>
@@ -35,11 +35,11 @@
                 <div class="grupo-formulario">
                     <label>quiero agradecerte</label>
                     <div class="contenedor-entrada">
-                        <input placeholder="Escribe aquí tu mensaje de agradecimiento..."></input>
+                        <input placeholder="Escribe aquí tu agradecimiento..."></input>
                     </div>
                 </div>
 
-                <button type="submit" class="boton-enviar">ENVIAR</button>
+                <button type="submit" class="boton">ENVIAR</button>
             </form>
         </div>
     </div>
---
+++ pruebas/okformulario.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario Enviado</title>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+    <main class="tarjeta" id="okPagina">
+        <h1>Formulario enviado correctamente</h1>
+        <a href="veragradecimientos.php"><button class="boton">Regresar</button></a>
+    </main>
+</body>
+</html>
